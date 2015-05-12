190a6545f2e58caf7bbbed31ee56758fa104f817
Tom Oberhauser <tom@devfoo.de>
2015-05-12 16:09:56 +0200

    Uglify is working.

--- uebung03/Gruntfile.js
+++ uebung03/Gruntfile.js
@@ -1,10 +1,14 @@
 /**
  * MME2 - Uebung 03 - Tom Oberhauser 798158
  * Tasks:
- * - execute frisby test
- * - minify server.js
- * - copy minified server.js into folder build
- * - copy src/public into folder build
+ * [ ] execute frisby test
+ * [x] minify server.js
+ * [x] copy minified server.js into folder build
+ * [] copy src/public into folder build
+ */
+
+/**
+ * USE grunt-jasmine-node and grunt-copy
  */
 
 module.exports = function(grunt) {
@@ -20,8 +24,10 @@ module.exports = function(grunt) {
          * (not really necessary here since i only have one file...)
          */
         concat: {
-            src: 'src/*.js', //source files
-            dest: 'build/<%= pkg.name %>.js' //destination files, filename taken out of package.json
+            dist: {
+                src: 'src/*.js', //source files
+                dest: 'build/<%= pkg.name %>.js' //destination files, filename taken out of package.json
+            }
         },
 
         /**
@@ -30,13 +36,41 @@ module.exports = function(grunt) {
         uglify: {
             options: {
                 // the banner is inserted at the top of the output
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n',
+                sourceMap: true,
+                compress: {}, //must be empty object, not true https://github.com/gruntjs/grunt-contrib-uglify/issues/298
+                mangle: {}
             },
             dist: {
                 files: {
                     'build/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>'] //minify concat output
                 }
             }
+        },
+
+        /**
+         * test configuration
+         */
+        frisby: {
+            options: {
+                forceExit: true,
+                match: '.',
+                matchall: false,
+                extensions: 'js',
+                specNameMatcher: 'spec'
+            },
+            all: ['spec/']
+        },
+
+        /**
+         * grunt-copy
+         */
+        copy: {
+            main: {
+                  expand: true,
+                  src: 'src/public/**',
+                  dest: 'build/public/'
+            },
         }
     });
     /**
@@ -45,5 +79,9 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-jasmine-node');
+    grunt.loadNpmTasks('grunt-copy');
+    grunt.registerTask('test',['frisby']);
     grunt.registerTask('default', ['concat','uglify']);
+    // grunt.registerTask('default', ['concat','uglify','copy']);
 };
