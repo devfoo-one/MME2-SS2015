f343b90cdfcb0a34eb4e5430fd29af80b72ceca5
Tom Oberhauser <tom@devfoo.de>
2015-05-12 19:43:45 +0200

    Copy and test is working

--- uebung03/Gruntfile.js
+++ uebung03/Gruntfile.js
@@ -1,57 +1,41 @@
 /**
  * MME2 - Uebung 03 - Tom Oberhauser 798158
  * Tasks:
- * [ ] execute frisby test
+ * [x] execute frisby test
  * [x] minify server.js
  * [x] copy minified server.js into folder build
- * [] copy src/public into folder build
+ * [x] copy src/public into folder build
  */
 
-/**
- * USE grunt-jasmine-node and grunt-copy
- */
 
 module.exports = function(grunt) {
     // initialize configObject
     grunt.config.init({
-        /**
-         * read package specs from package.json
-         */
-        pkg: grunt.file.readJSON('package.json'),
+
+        pkg: grunt.file.readJSON('package.json'), // read package specs from package.json
 
         /**
-         * concatenation of all .js files into one file
-         * (not really necessary here since i only have one file...)
-         */
-        concat: {
-            dist: {
-                src: 'src/*.js', //source files
-                dest: 'build/<%= pkg.name %>.js' //destination files, filename taken out of package.json
-            }
-        },
-
-        /**
-         * configure minification
+         * minification
          */
         uglify: {
             options: {
                 // the banner is inserted at the top of the output
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n',
-                sourceMap: true,
+                sourceMap: false, //create no map file
                 compress: {}, //must be empty object, not true https://github.com/gruntjs/grunt-contrib-uglify/issues/298
                 mangle: {}
             },
-            dist: {
+            build: {
                 files: {
-                    'build/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>'] //minify concat output
+                    'build/<%= pkg.name %>.min.js': ['src/*.js'] //minify all js files in one file
                 }
             }
         },
 
         /**
-         * test configuration
+         * jasmine_node (frisby) test
          */
-        frisby: {
+        jasmine_node: {
             options: {
                 forceExit: true,
                 match: '.',
@@ -66,22 +50,26 @@ module.exports = function(grunt) {
          * grunt-copy
          */
         copy: {
-            main: {
-                  expand: true,
-                  src: 'src/public/**',
-                  dest: 'build/public/'
-            },
+            build: {
+                files: [{
+                       expand: true,
+                       cwd: 'src/public',
+                       src: ['**/*.js','**/*.htm','**/*.css'], //https://github.com/cbas/grunt-rev/issues/29
+                       //without file endings, grunt seems to interpret folders as files
+                       dest: 'build/public/'
+                }]
+            }
         }
     });
+
     /**
-     * load required grunt-modules. These should have been installen through npm
+     * load required grunt-modules. These should have been installed through npm
      */
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-jasmine-node');
     grunt.loadNpmTasks('grunt-copy');
-    grunt.registerTask('test',['frisby']);
-    grunt.registerTask('default', ['concat','uglify']);
-    // grunt.registerTask('default', ['concat','uglify','copy']);
+
+    grunt.registerTask('default', ['jasmine_node','uglify','copy']); //if jasmine_node fails, everything fails
 };
